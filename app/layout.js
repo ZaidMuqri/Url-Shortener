8769d78f280048d96c26863eb1155613ad637cde
ZaidMuqri <zaidmuqri12@gmail.com>
2025-10-17 21:05:43 +0530

    Imported Script in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Script from "next/script";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
